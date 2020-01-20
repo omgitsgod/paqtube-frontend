99207577a00b620dbf62f0f0fe33b6a13e1c0aff
omgitsgod <omgitsgod@gmail.com>
2020-01-19 23:09:09 -0500

    Cleanup/formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,16 @@ import '../css/Card.css'
 function Card(props) {
   return (
     <div className='container'>
-  <div className='card'>
-    <img src={props.img} alt='Project' className='card_image' />
-    <div className='card_text'>
-      <h2 className='title'>{props.name}</h2>
-    </div>
+      <div className='card'>
+        <img src={props.img} alt='Project' className='card_image' />
+        <div className='card_text'>
+          <h2 className='title'>{props.name}</h2>
+        </div>
     <hr />
     <div className='card_footer'>
-    <div className='summary'>
-      {props.text}
-    </div>
+      <div className='summary'>
+        {props.text}
+      </div>
     </div>
   </div>
 </div>
