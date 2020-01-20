20fa2d56a9cf3d54748d4959d9738460ae854fcb
omgitsgod <omgitsgod@gmail.com>
2020-01-19 23:40:01 -0500

    Cleanup/formatting

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../css/Banner.css'
-import paqBanner from '../paqBanner.jpg'
+import '../css/Banner.css';
+import paqBanner from '../paqBanner.jpg';
 
 function Banner(props) {
   return (
