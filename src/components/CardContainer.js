a0e8329dc9fa4f055a8ff1fce123d840dab2a259
omgitsgod <omgitsgod@gmail.com>
2020-01-19 23:06:13 -0500

    Cleanup/formatting

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import '../css/CardContainer.css'
 import Card from './Card'
-import paq from '../paq.jpg'
 import Icon from '@mdi/react'
 import {mdiArrowLeftCircle,mdiArrowRightCircle} from '@mdi/js'
 
@@ -13,7 +12,6 @@ function CardContainer(props) {
 
   useEffect(()=>{
     fetch('http://localhost:5000').then(r=>r.json()).then(json => {
-      console.log(json);
       setNextPageToken(json.nextPageToken || '')
       setPrevPageToken(json.prevPageToken || '')
       setVideos(json.items.map(vid => <a href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>))
@@ -22,7 +20,6 @@ function CardContainer(props) {
 
   const getNext = (token) => {
     fetch(`http://localhost:5000/page/${token}`).then(r=>r.json()).then(json => {
-      console.log(json);
       setNextPageToken(json.nextPageToken || '')
       setPrevPageToken(json.prevPageToken || '')
       setVideos(json.items.map(vid => <a href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>))
@@ -31,9 +28,8 @@ function CardContainer(props) {
 
   const getPrev = (token) => {
     fetch(`http://localhost:5000/page/${token}`).then(r=>r.json()).then(json => {
-      console.log(json);
-        setNextPageToken(json.nextPageToken || '')
-        setPrevPageToken(json.prevPageToken || '')
+      setNextPageToken(json.nextPageToken || '')
+      setPrevPageToken(json.prevPageToken || '')
       setVideos(json.items.map(vid => <a href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>))
     })
   }
@@ -41,13 +37,13 @@ function CardContainer(props) {
 
   return (
     <div>
-    <div className='cards'>
-      {videos || 'loading!'}
-    </div>
-    <div className='arrows'>
-    {prevPageToken ? <Icon path={mdiArrowLeftCircle} color='white' size={2} onClick={()=>getPrev(prevPageToken)}/> : null}
-    {nextPageToken ? <Icon path={mdiArrowRightCircle} color='white' size={2} onClick={()=>getNext(nextPageToken)}/> : null}
-    </div>
+      <div className='cards'>
+        {videos || 'loading!'}
+      </div>
+      <div className='arrows'>
+        {prevPageToken ? <Icon path={mdiArrowLeftCircle} color='white' size={2} onClick={()=>getPrev(prevPageToken)}/> : null}
+        {nextPageToken ? <Icon path={mdiArrowRightCircle} color='white' size={2} onClick={()=>getNext(nextPageToken)}/> : null}
+      </div>
     </div>
   );
 }
