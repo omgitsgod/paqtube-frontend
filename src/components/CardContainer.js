7df81dd3ea9e2736c870d0a11c884bca34dcd7f3
omgitsgod <omgitsgod@gmail.com>
2020-01-22 11:34:34 -0500

    Update to make compatible with netlify prod

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -9,17 +9,18 @@ function CardContainer(props) {
   const [videos, setVideos] = useState([]);
   const [nextPageToken, setNextPageToken] = useState('');
   const [prevPageToken, setPrevPageToken] = useState('');
+  const host = process.env.REACT_APP_HOST || 'http://localhost:5000'
 
   useEffect(()=>{
-    fetch('http://localhost:5000').then(r=>r.json()).then(json => {
+    fetch(host).then(r=>r.json()).then(json => {
       setNextPageToken(json.nextPageToken || '');
       setPrevPageToken(json.prevPageToken || '');
       setVideos(json.items.map(vid => <a key={vid.id.videoId} href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>));
     })
-  }, [])
+  }, [host])
 
   const getNext = (token) => {
-    fetch(`http://localhost:5000/page/${token}`).then(r=>r.json()).then(json => {
+    fetch(`${host}/page/${token}`).then(r=>r.json()).then(json => {
       setNextPageToken(json.nextPageToken || '');
       setPrevPageToken(json.prevPageToken || '');
       setVideos(json.items.map(vid => <a key={vid.id.videoId} href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>));
@@ -28,7 +29,7 @@ function CardContainer(props) {
   }
 
   const getPrev = (token) => {
-    fetch(`http://localhost:5000/page/${token}`).then(r=>r.json()).then(json => {
+    fetch(`${host}/page/${token}`).then(r=>r.json()).then(json => {
       setNextPageToken(json.nextPageToken || '');
       setPrevPageToken(json.prevPageToken || '');
       setVideos(json.items.map(vid => <a key={vid.id.videoId} href={'https://www.youtube.com/watch?v='+vid.id.videoId} target='blank'><Card key={vid.id.videoId} name={vid.snippet.title} text={vid.snippet.description} img={vid.snippet.thumbnails.high.url}/></a>));
@@ -40,7 +41,7 @@ function CardContainer(props) {
   return (
     <div>
       <div className='cards'>
-        {videos || 'loading!'}
+        {videos}
       </div>
       <div className='arrows'>
         {prevPageToken ? <Icon path={mdiArrowLeftCircle} color='white' size={2} onClick={()=>getPrev(prevPageToken)}/> : null}
